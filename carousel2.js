aff8ea5c296716b26d193f0051a143436c8b397e
jan miksik <jan.miksik@email.cz>
2020-04-25 11:53:12 +0200

    add carousel2 and fix font-family to Times New

--- carousel2.js
+++ carousel2.js
@@ -1,169 +1,169 @@
-var carousel = document.querySelector('.carousel');
-var carouselContent = document.querySelector('.carousel-content');
-var slides = document.querySelectorAll('.slide');
-var arrayOfSlides = Array.prototype.slice.call(slides);
-var carouselDisplaying;
+var carousel2 = document.querySelector('.carousel2');
+var carouselContent2 = document.querySelector('.carousel-content2');
+var slides2 = document.querySelectorAll('.slide2');
+var arrayOfSlides2 = Array.prototype.slice.call(slides2);
+var carouselDisplaying2 = 1;
 var screenSize;
-setScreenSize();
-var lengthOfSlide;
+setScreenSize2();
+var lengthOfSlide2;
 
-function addClone() {
-  var lastSlide = carouselContent.lastElementChild.cloneNode(true);
-  lastSlide.style.left = -lengthOfSlide + 'px';
-  carouselContent.insertBefore(lastSlide, carouselContent.firstChild);
+function addClone2() {
+  var lastSlide2 = carouselContent2.lastElementChild.cloneNode(true);
+  lastSlide2.style.left = -lengthOfSlide2 + 'px';
+  carouselContent2.insertBefore(lastSlide2, carouselContent2.firstChild);
 }
-// addClone();
+// addClone2();
 
-function removeClone() {
-  var firstSlide = carouselContent.firstElementChild;
-  firstSlide.parentNode.removeChild(firstSlide);
+function removeClone2() {
+  var firstSlide2 = carouselContent2.firstElementChild;
+  firstSlide2.parentNode.removeChild(firstSlide2);
 }
 
-function moveSlidesRight() {
-  var slides = document.querySelectorAll('.slide');
-  var slidesArray = Array.prototype.slice.call(slides);
-  var width = 0;
+function moveSlidesRight2() {
+  var slides2 = document.querySelectorAll('.slide2');
+  var slidesArray2 = Array.prototype.slice.call(slides2);
+  var width2 = 0;
 
-  slidesArray.forEach(function (el, i) {
-    el.style.left = width + 'px';
-    width += lengthOfSlide;
+  slidesArray2.forEach(function (el, i) {
+    el.style.left = width2 + 'px';
+    width2 += lengthOfSlide2;
   });
-  addClone();
+  addClone2();
 }
-moveSlidesRight();
+moveSlidesRight2();
 
-function moveSlidesLeft() {
-  var slides = document.querySelectorAll('.slide');
-  var slidesArray = Array.prototype.slice.call(slides);
-  slidesArray = slidesArray.reverse();
-  var maxWidth = (slidesArray.length - 1) * lengthOfSlide;
+function moveSlidesLeft2() {
+  var slides2 = document.querySelectorAll('.slide2');
+  var slidesArray2 = Array.prototype.slice.call(slides2);
+  slidesArray2 = slidesArray2.reverse();
+  var maxWidth2 = (slidesArray2.length - 1) * lengthOfSlide2;
 
-  slidesArray.forEach(function (el, i) {
-    maxWidth -= lengthOfSlide;
-    el.style.left = maxWidth + 'px';
+  slidesArray2.forEach(function (el, i) {
+    maxWidth2 -= lengthOfSlide2;
+    el.style.left = maxWidth2 + 'px';
   });
 }
 
-window.addEventListener('resize', setScreenSize);
+window.addEventListener('resize', setScreenSize2);
 
-function setScreenSize() {
+function setScreenSize2() {
   if (window.innerWidth >= 500) {
-    carouselDisplaying = 1;
+    carouselDisplaying2 = 1;
   } else if (window.innerWidth >= 300) {
-    carouselDisplaying = 1;
+    carouselDisplaying2 = 1;
   } else {
-    carouselDisplaying = 1;
+    carouselDisplaying2 = 1;
   }
-  getScreenSize();
+  getScreenSize2();
 }
 
-function getScreenSize() {
-  var slides = document.querySelectorAll('.slide');
-  var slidesArray = Array.prototype.slice.call(slides);
-  lengthOfSlide = carousel.offsetWidth / carouselDisplaying;
-  var initialWidth = -lengthOfSlide;
-  slidesArray.forEach(function (el) {
-    el.style.width = lengthOfSlide + 'px';
-    el.style.left = initialWidth + 'px';
-    initialWidth += lengthOfSlide;
+function getScreenSize2() {
+  var slides2 = document.querySelectorAll('.slide2');
+  var slidesArray2 = Array.prototype.slice.call(slides2);
+  lengthOfSlide2 = carousel2.offsetWidth / carouselDisplaying2;
+  var initialWidth2 = -lengthOfSlide2;
+  slidesArray2.forEach(function (el) {
+    el.style.width = lengthOfSlide2 + 'px';
+    el.style.left = initialWidth2 + 'px';
+    initialWidth2 += lengthOfSlide2;
   });
 }
 
-var rightNav = document.querySelector('.nav-right');
-rightNav.addEventListener('click', moveLeft);
+var rightNav2 = document.querySelector('.nav-right2');
+rightNav2.addEventListener('click', moveLeft2);
 
-var moving = true;
-function moveRight() {
-  if (moving) {
-    moving = false;
-    var lastSlide = carouselContent.lastElementChild;
-    lastSlide.parentNode.removeChild(lastSlide);
-    carouselContent.insertBefore(lastSlide, carouselContent.firstChild);
-    removeClone();
-    var firstSlide = carouselContent.firstElementChild;
-    firstSlide.addEventListener('transitionend', activateAgain);
-    moveSlidesRight();
+var moving2 = true;
+function moveRight2() {
+  if (moving2) {
+    moving2 = false;
+    var lastSlide2 = carouselContent2.lastElementChild;
+    lastSlide2.parentNode.removeChild(lastSlide2);
+    carouselContent2.insertBefore(lastSlide2, carouselContent2.firstChild);
+    removeClone2();
+    var firstSlide2 = carouselContent2.firstElementChild;
+    firstSlide2.addEventListener('transitionend', activateAgain2);
+    moveSlidesRight2();
   }
 }
 
-function activateAgain() {
-  var firstSlide = carouselContent.firstElementChild;
-  moving = true;
-  firstSlide.removeEventListener('transitionend', activateAgain);
+function activateAgain2() {
+  var firstSlide2 = carouselContent2.firstElementChild;
+  moving2 = true;
+  firstSlide2.removeEventListener('transitionend', activateAgain2);
 }
 
-var leftNav = document.querySelector('.nav-left');
-leftNav.addEventListener('click', moveRight);
+var leftNav2 = document.querySelector('.nav-left2');
+leftNav2.addEventListener('click', moveRight2);
 
 // var moveLeftAgain = true;
 
-function moveLeft() {
-  if (moving) {
-    moving = false;
-    removeClone();
-    var firstSlide = carouselContent.firstElementChild;
-    firstSlide.addEventListener('transitionend', replaceToEnd);
-    moveSlidesLeft();
+function moveLeft2() {
+  if (moving2) {
+    moving2 = false;
+    removeClone2();
+    var firstSlide2 = carouselContent2.firstElementChild;
+    firstSlide2.addEventListener('transitionend', replaceToEnd2);
+    moveSlidesLeft2();
   }
 }
 
-function replaceToEnd() {
-  var firstSlide = carouselContent.firstElementChild;
-  firstSlide.parentNode.removeChild(firstSlide);
-  carouselContent.appendChild(firstSlide);
-  firstSlide.style.left = (arrayOfSlides.length - 1) * lengthOfSlide + 'px';
-  addClone();
-  moving = true;
-  firstSlide.removeEventListener('transitionend', replaceToEnd);
+function replaceToEnd2() {
+  var firstSlide2 = carouselContent2.firstElementChild;
+  firstSlide2.parentNode.removeChild(firstSlide2);
+  carouselContent2.appendChild(firstSlide2);
+  firstSlide2.style.left = (arrayOfSlides2.length - 1) * lengthOfSlide2 + 'px';
+  addClone2();
+  moving2 = true;
+  firstSlide2.removeEventListener('transitionend', replaceToEnd2);
 }
 
-carouselContent.addEventListener('mousedown', seeMovement);
+carouselContent2.addEventListener('mousedown', seeMovement2);
 
-var initialX;
-var initialPos;
-function seeMovement(e) {
-  initialX = e.clientX;
+var initialX2;
+var initialPos2;
+function seeMovement2(e) {
+  initialX2 = e.clientX;
   getInitialPos();
-  carouselContent.addEventListener('mousemove', slightMove);
-  document.addEventListener('mouseup', moveBasedOnMouse);
+  carouselContent2.addEventListener('mousemove', slightMove2);
+  document.addEventListener('mouseup', moveBasedOnMouse2);
 }
 
-function slightMove(e) {
-  if (moving) {
-    var movingX = e.clientX;
-    var difference = initialX - movingX;
-    if (Math.abs(difference) < lengthOfSlide / 4) {
-      slightMoveSlides(difference);
+function slightMove2(e) {
+  if (moving2) {
+    var moving2X = e.clientX;
+    var difference2 = initialX2 - moving2X;
+    if (Math.abs(difference2) < lengthOfSlide2 / 4) {
+      slightMoveSlides2(difference2);
     }
   }
 }
 
 function getInitialPos() {
-  var slides = document.querySelectorAll('.slide');
-  var slidesArray = Array.prototype.slice.call(slides);
-  initialPos = [];
-  slidesArray.forEach(function (el) {
+  var slides2 = document.querySelectorAll('.slide2');
+  var slidesArray2 = Array.prototype.slice.call(slides2);
+  initialPos2 = [];
+  slidesArray2.forEach(function (el) {
     var left = Math.floor(parseInt(el.style.left.slice(0, -2)));
-    initialPos.push(left);
+    initialPos2.push(left);
   });
 }
 
-function slightMoveSlides(newX) {
-  var slides = document.querySelectorAll('.slide');
-  var slidesArray = Array.prototype.slice.call(slides);
-  slidesArray.forEach(function (el, i) {
-    var oldLeft = initialPos[i];
-    el.style.left = oldLeft + newX + 'px';
+function slightMoveSlides2(newX) {
+  var slides2 = document.querySelectorAll('.slide2');
+  var slidesArray2 = Array.prototype.slice.call(slides2);
+  slidesArray2.forEach(function (el, i) {
+    var oldLeft2 = initialPos2[i];
+    el.style.left = oldLeft2 + newX + 'px';
   });
 }
 
-function moveBasedOnMouse(e) {
+function moveBasedOnMouse2(e) {
   var finalX = e.clientX;
-  if (initialX - finalX > 0) {
-    moveRight();
-  } else if (initialX - finalX < 0) {
-    moveLeft();
+  if (initialX2 - finalX > 0) {
+    moveRight2();
+  } else if (initialX2 - finalX < 0) {
+    moveLeft2();
   }
-  document.removeEventListener('mouseup', moveBasedOnMouse);
-  carouselContent.removeEventListener('mousemove', slightMove);
+  document.removeEventListener('mouseup', moveBasedOnMouse2);
+  carouselContent2.removeEventListener('mousemove', slightMove2);
 }
